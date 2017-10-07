6dcc93c83704823340890012f0bda076f6cbc4e7
MalgoKra <32552555+MalgoKra@users.noreply.github.com>
2017-10-07 16:22:08 +0200

    zadanie 2 pkt. 7

--- JS/app.js
+++ JS/app.js
@@ -9,11 +9,30 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log(dropdown);
     
     forDropdown.addEventListener('mouseover', function() {
-        dropdown.style.display = 'block';
+    dropdown.style.display = 'block';
     });
     
     forDropdown.addEventListener('mouseout', function() {
     dropdown.style.display = 'none';
+    });
+    
+    //Zadanie 2
+    var buttons = document.querySelectorAll('.read-more');
+    console.log(buttons);
+    
+    console.log(buttons[0].previousElementSibling);
+    
+    buttons[0].addEventListener('click', function() {
+    console.log('działa'); 
+    });
+    buttons[0].addEventListener('click', function() {
+    console.log(this); 
+    });
+    buttons[0].addEventListener('click', function() {
+    var textArea = this.previousElementSibling;
+    textArea.style.display = 'block';
+    console.log(textArea); 
+    console.log(this.innerHTML); 
     });
     
 });
